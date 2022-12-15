9abd4f67dbb6f709a30ef7a91cacc32501f9fbe4
My Name <home@example.com>
2022-12-15 21:47:00 +0100

    code paint de base fonctionnel (panel de couleurs et outils); commenté! je dois maintenant ajouté les formes et gérer le responsive

---
+++ projet_Paint/js/Dessin.js
@@ -0,0 +1,90 @@
+class Dessin{
+    constructor(){
+
+        let isDrawing = false;
+        let x = 0;
+        let y = 0;
+        let sheet = document.querySelector("#feuille");
+
+        this.ctx = sheet.getContext("2d");
+        this.ctx.lineWidth = 2;
+
+//J'écoute les différents événement de utilisateur pour dessiner
+
+        sheet.addEventListener("mousedown", e =>{
+            isDrawing = true;
+            x = e.offsetX;
+            y = e.offsety;
+        });
+
+        sheet.addEventListener('mousemove', e =>{
+            if (isDrawing){
+                this.drawLine(this.ctx, x, y, e.offsetX, e.offsetY);
+                x = e.offsetX;
+                y = e.offsetY;
+            };
+        });
+
+        sheet.addEventListener('mouseup', e =>{
+            if (isDrawing){
+                this.drawLine(this.ctx, x, y, e.offsetX, e.offsetY);
+                x = 0;
+                y = 0;
+                isDrawing = false;
+            };
+        });
+
+        sheet.addEventListener('mouseout', e =>{
+            if (isDrawing){
+                this.drawLine(this.ctx, x, y, e.offsetX, e.offsetY);
+                x = 0;
+                y = 0;
+                isDrawing = false;
+            };
+        });
+    };
+
+//Fonction pour déssiner
+
+    drawLine(ctx, x1, y1, x2, y2){
+        ctx.beginPath();
+        ctx.strokeStyle = this.setColor();
+        ctx.lineWidth = this.ctx.lineWidth;
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+        ctx.closePath();
+    };
+
+//Fonction pour le changement de couleurs
+
+    setColor(color){
+        this.ctx.strokeStyle = color;
+    };
+
+//Fonction plus gros trait
+
+    upSizing(){
+        this.ctx.lineWidth ++;
+    };
+
+//Fonction plus petit trait 
+
+// --/-- écrite en ternaire --/-- prends moins de place mais apprends à marché avant de vouloir courir !!!!! (à utiliser seulement pour des conditions unique / simple)
+//stucture d'une condition ternaire : condition ? if : else ; 
+
+    // downSizing(){
+    //     if(this.ctx.lineWidth < 1){
+    //         this.ctx.lineWidth = 1;
+    //     } this.ctx.lineWidth --;
+    // };
+
+     downSizing(){this.ctx.lineWidth < 1 ? this.ctx.lineWidth = 1 : this.ctx.lineWidth --;
+    };
+
+//Fonction effacer 
+
+    erase(){
+        this.ctx.clearRect(0, 0, innerWidth, innerHeight);
+    };
+};
--- projet_Paint/js/script.js
+++ projet_Paint/js/script.js
@@ -1,86 +1,44 @@
 // création des différentes partie html de la page
-let struturHTML = document.querySelector("body")
-struturHTML.innerHTML = `
-    <header class="drawingSite">
-        <h1>Super site de dessin en ligne</h1>
-    </header>
-    <main class="drawingMain">
-        <section class="drawingMain-area Area">
-            <nav class="Area-drawingTools">
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </nav>
-            <div class="Area-drawing">
-                <canvas id="drawingArea" width="300px" height="300px">Désolé, votre navigateur ne prend pas en charge CANVAS...</canvas>
-            </div>
-        </section>
-        <section class="">
-        </section>
-    <footer class="drawingFooter">
-        <p class="footer"></p>
-    </footer>
-`;
+// let struturHTML = document.querySelector("body")
+// struturHTML.innerHTML = `
 
-// déclaration de variables & constante global
+// `;
 
-const myDraw = document.querySelector('#drawingArea');
-const ctx = myDraw.getContext('2d');
+// Création de mon objet canvas de la classe Dessin
 
-let isDrawing = false;
-let x = 0;
-let y = 0;
+let drawArea = new Dessin("#feuille");  
 
-// let colorLine = ;
-// let thicknessLine = ;
+// J'attribut le background suivant les couleurs choisit dans les <div> de mes couleurs prédéfinit
 
-// déclaration des fonctions
+document.querySelectorAll("#palette div").forEach(color => {
+        color.style.backgroundColor = color.dataset.color;
+        
+        //  Ici "j'écoute" la sélection de couleur pour modifier la couleur du pinceau
 
+        color.addEventListener("click", () => {
+            drawArea.setColor(color.dataset.color);
+        });
+    });
 
+// Déclaration des fonctions
 
-// écoute des événements
+// Ecoute des événements
 
-
-
-//______________________________________TEST____________________________________//
-
-// à essayer d'ecrir sous forme de fonction non fléchée, à voir comment faire avec "l'événement "
-
-myDraw.addEventListener('mousedown', e => {
-    x = e.offsetX;
-    y = e.offsetY;
-    isDrawing = true;
-});
-// à essayer d'ecrir sous forme de fonction non fléchée, à voir comment faire avec "l'événement "
-
-myDraw.addEventListener('mousemove', e => {
-    if (isDrawing === true) {
-        drawLine(ctx, x, y, e.offsetX, e.offsetY);
-        x = e.offsetX;
-        y = e.offsetY;
-    };
-});
-// à essayer d'ecrir sous forme de fonction non fléchée
-
-window.addEventListener('mouseup', e => {
-    if (isDrawing === true) {
-        drawLine(ctx, x, y, e.offsetX, e.offsetY);
-        x = 0;
-        y = 0;
-        isDrawing = false;
-    };
+document.querySelector("#plus").addEventListener("click", () => {
+    drawArea.upSizing();
 });
 
-function drawLine(ctx, x1, y1, x2, y2) {
-    ctx.beginPath();
-//  ajouter une variable pour que l'utilisateur choisise sa couleur
-    ctx.strokeStyle = 'black';
-//  ajouter une variable pour que l'utilisateur choisise sa taille de pinceau en pixel
-    ctx.lineWidth = 10;
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.closePath();
-}
+document.querySelector("#moins").addEventListener("click", () => {
+    drawArea.downSizing();
+});
+
+document.querySelector("#gomme").addEventListener("click", () => {
+    drawArea.setColor("white");
+});
+
+document.querySelector("#effacer").addEventListener("click", () => {
+    drawArea.erase();
+});
+
+//  test canvas responsive
+
